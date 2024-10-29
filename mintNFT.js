52e92ddff5323b528d5a7094118567ca6cfea970
seanblatter <63274835+seanblatter@users.noreply.github.com>
2024-10-28 20:50:18 -0500

    Update mintNFT.js

--- mintNFT.js
+++ mintNFT.js
@@ -1,7 +1,7 @@
-const apiKey = "sk_staging_36ZTndrQJB4SMDkQYrhag8TakSYHGDJyEgw95QeQLnswG62pMNy5Zt8tpzwf2pode4FHEGjy5cyoYVtEWfsrWMtPsZ4vj4NKh74eLhNu4txL3b14Zz4ukkmX8cb2eAja2i9dPoubdBFtjr6tSjJic3jhojaumgWrJvgkC68WJkuZhe6jNRRQv3jafnVNU3MhqCch9UvJHdp8BkqDhLNhNzf";
+const apiKey = "sk_staging_36ZTidrQJB4SMDkQYrhag8TakSYHGDJyEgw95QeQLnswG62pMNy5Zt8tpzwf2pode4FHEGjy5cyoYVtEWfsrWMtPsZ4vj4NKh74eLhNu4txL3b14Zz4ukkmX8cb2eAja2i9dPoubdBFtjr6tSjJic3jhojaumgWrJvgkC68WJkuZhe6jNRRQv3jafnVNU3MhqCch9UvJHdp8BkqDhLNhNzf";
 const chain = "solana"; // or "polygon-amoy", "ethereum-sepolia", ...
 const env = "staging"; // or "www"
-const recipientEmail = "blattersean@outlook.com";
+const recipientEmail = "example@outlook.com";
 //const walletAddress = 'DAzkMX5p77bqwGQxdArAUU4uit9QR3r4MeLDaUztA1yo'
 const recipientAddress = `email:${recipientEmail}:${chain}`;
 
